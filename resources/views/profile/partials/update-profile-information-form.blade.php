57f35e1ceda39226321f8645c79c5e796f127a65
dungldt <langdinhthanhdung@gmail.com>
2023-11-07 09:48:08 +0700

    seperate partial user avatar

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,9 +13,15 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" >
         @csrf
         @method('patch')
+
+        <div>
+            <x-input-label for="name" :value="__('Avatar')" />
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div>
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
---
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -0,0 +1,19 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('User avatar') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __("Add or Update user avatar") }}
+        </p>
+    </header>
+        <div>
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div>
+        <div class="flex items-center gap-4 mt-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+        </div>
+    </form>
+</section>
