7eed3204023c7c96473b7f3761baeab182a12132
BBMCode <bbmcode247@gmail.com>
2023-11-08 09:40:15 +0700

    try to validate image in controller

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\RedirectResponse;
 
 class AvatarController extends Controller
 {
-    //
+    /**
+     * Update user avatar
+     */
+    public function update(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'avatar' => 'required|image',
+        ]);
+
+        return Redirect::route('profile.edit')->with('status', 'Avatar updated!');
+    }
 }
