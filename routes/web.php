f4bfdd3518cc6032bc84c459745aa6ed23ce988b
BBMCode <bbmcode247@gmail.com>
2023-11-08 12:14:56 +0700

    try to call openai API

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Profile\AvatarController;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +29,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar.update');
+});
+
+Route::get('/openai', function () {
+    $result = OpenAI::images()->create([
+        'prompt' => "Fantasy Character Avatar",
+        'size' => "512x512",
+        'quality' => "standard",
+        'n' => 1,
+    ]);
+
+    return response(['url' => $result->data[0]->url]);
 });
 
 require __DIR__.'/auth.php';
